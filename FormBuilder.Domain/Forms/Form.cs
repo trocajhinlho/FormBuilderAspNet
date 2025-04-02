f6b468c836b3c7c03e9317c91ba0bca3250da6c2
trocajhinlho <tversuri@gmail.com>
2025-04-02 16:53:41 -0300

    Add ShortId package for Form public Id generation

--- FormBuilder.Domain/Forms/Form.cs
+++ FormBuilder.Domain/Forms/Form.cs
@@ -1,4 +1,7 @@
 ﻿using FormBuilder.Domain.Shared;
+using shortid;
+using shortid.Configuration;
+using System.Reflection.Metadata.Ecma335;
 
 namespace FormBuilder.Domain.Forms;
 
@@ -30,7 +33,8 @@ public class Form : IAuditable
 
     private string CreatePublicId()
     {
-        return "public_id";
+        var options = new GenerationOptions(useSpecialCharacters: false, length: 8);
+        return ShortId.Generate(options);
     }
 
 }

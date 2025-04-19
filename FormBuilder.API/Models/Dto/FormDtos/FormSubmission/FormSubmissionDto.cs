a5ceaee37e8cce2b95993c35f2d38e1f62b25e81
trocajhinlho <tversuri@gmail.com>
2025-04-19 17:57:31 -0300

    Add Form Submission detail Dto

---
+++ FormBuilder.API/Models/Dto/FormDtos/FormSubmission/FormSubmissionDto.cs
@@ -0,0 +1,4 @@
+﻿namespace FormBuilder.API.Models.Dto.FormDtos.FormSubmission;
+
+public record FormSubmissionDto(DateTime SubmittedOn, IEnumerable<QuestionAnswerDto> Answers);
+
---
+++ FormBuilder.API/Models/Dto/FormDtos/FormSubmission/QuestionAnswerDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FormBuilder.API.Models.Dto.FormDtos.FormSubmission;
+
+public record QuestionAnswerDto(
+    Guid Id,
+    string Label,
+    string Answer,
+    bool IsRequired,
+    bool IsDeleted);
+

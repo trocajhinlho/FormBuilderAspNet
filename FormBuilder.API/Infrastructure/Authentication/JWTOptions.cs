cdb0674d86c9b89df7dae94e88d0e07becbcd51c
trocajhinlho <tversuri@gmail.com>
2025-04-22 22:34:40 -0300

    Add JWTOption object and appsettings configuration

---
+++ FormBuilder.API/Infrastructure/Authentication/JWTOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace FormBuilder.API.Infrastructure.Authentication;
+
+public class JWTOptions
+{
+    public string PrivateKey { get; init; }
+    public string Issuer { get; init; }
+
+    public string Audience { get; init; }
+    public int ExpirationMinutes { get; init; }
+}

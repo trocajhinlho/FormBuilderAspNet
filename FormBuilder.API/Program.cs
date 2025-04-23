bedaebe23b62faf6e36a465e63d503ffd865e082
trocajhinlho <tversuri@gmail.com>
2025-04-22 22:35:10 -0300

    Add AspNet Identity and JWT configuration

--- FormBuilder.API/Program.cs
+++ FormBuilder.API/Program.cs
@@ -1,8 +1,14 @@
 using FormBuilder.API.Commands.Forms;
+using FormBuilder.API.Infrastructure.Authentication;
 using FormBuilder.API.Infrastructure.Middleware;
 using FormBuilder.API.Service;
 using FormBuilder.Domain.Context;
+using FormBuilder.Domain.Users;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +18,8 @@ builder.Services.AddCors(options =>
         policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
     }));
 
+builder.Services.Configure<JWTOptions>(builder.Configuration.GetSection("JWT"));
+
 // Add services to the container.
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -19,6 +27,33 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
         sqlOptions => sqlOptions.MigrationsAssembly("FormBuilder.API"));
 });
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = false; 
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+})
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var jwt = builder.Configuration.GetSection("JWT").Get<JWTOptions>();
+
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.PrivateKey))
+        };
+    });
 
 builder.Services.AddScoped<IFormService, FormService>();
 builder.Services.AddScoped<IUpdateFormCommandHandler, UpdateFormCommandHandler>();
@@ -47,6 +82,8 @@ if (app.Environment.IsDevelopment())
 app.UseExceptionHandler();
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
---
+++ FormBuilder.Domain/Users/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FormBuilder.Domain.Users;
+
+public class ApplicationUser : IdentityUser<Guid>
+{
+ 
+}
